bd72c03dd67a47cff22942ff6d94c5d6e8d69428
Kamikadr <60061947+Kamikadr@users.noreply.github.com>
2023-12-11 02:19:36 +0700

    fix

--- Assets/Scripts/Common/Factory.cs
+++ Assets/Scripts/Common/Factory.cs
@@ -9,16 +9,14 @@ namespace ShootEmUp.Common
     {
         private readonly T _prefabItem;
         private readonly LifetimeScope _parentScope;
-        private readonly IObjectResolver _resolver;
 
-        public Factory(T prefabItem, LifetimeScope parentScope, IObjectResolver resolver)
+        public Factory(T prefabItem, LifetimeScope parentScope)
         {
             _prefabItem = prefabItem;
             _parentScope = parentScope;
-            _resolver = resolver;
         }
 
-        public  T Create(Transform parent)
+        public T Create(Transform parent)
         {
             using (LifetimeScope.EnqueueParent(_parentScope))
             {
@@ -26,9 +24,7 @@ namespace ShootEmUp.Common
 
                 return item;
             }
-            //var item = Object.Instantiate(_prefabItem, parent);
-
-            //return item;
         }
     }
+    
 }
--- Assets/Scripts/VContainer/SceneLifetimeScope.cs
+++ Assets/Scripts/VContainer/SceneLifetimeScope.cs
@@ -1,20 +1,130 @@
-using ShootEmUp.Bullets;
+using App;
+using Game;
+using ShootEmUp.Characters;
 using ShootEmUp.Common;
+using ShootEmUp.Componets;
 using ShootEmUp.Enemies;
 using ShootEmUp.Game;
+using ShootEmUp.GameInput;
+using ShootEmUp.Level;
+using UI;
+using UnityEngine;
+using UnityEngine.Serialization;
 using VContainer;
 using VContainer.Unity;
 
-namespace ShootEmUp.DI
+namespace ShootEmUp.Bullets
 {
-    public class SceneLifetimeScope : LifetimeScope
+    public class SceneLifetimeScope: LifetimeScope
     {
+        [Header("Game settings")]
+        [SerializeField] private TimerSettings startTimerSettings;
+        
+        [Header("Location of objects")]
+        [SerializeField] private Transform worldTransform;
+        [SerializeField] private Transform bulletPoolContainer;
+        [SerializeField] private Transform enemyPoolContainer;
+        
+        [Header("Prefabs of objects")]
+        [SerializeField] private Bullet bulletPrefab;
+        [SerializeField] private Enemy enemyPrefab;
+        [SerializeField] private Character character;
+
+        [Header("Enemy positions")] 
+        [SerializeField] private Transform[] enemySpawnPositions;
+        [SerializeField] private Transform[] enemyFirePositions;
+
+        [Space]
+        [SerializeField] private float enemySpawnDelay;
+        [SerializeField] private int enemyMaxCount;
+
+        [Header("Environment")] 
+        [SerializeField] private Transform backgroundTransform;
+        [SerializeField] private BackgroundMoveConfig backgroundMoveConfig;
+        [SerializeField] private BulletConfig playerBulletConfig;
+
+        [Header("UI")]
+        [SerializeField] private PauseResumeButtonStateController pauseResumeButtonStateController;
+        [SerializeField] private PauseResumeButtonListener pauseResumeButtonListener;
+        [SerializeField] private StartButtonListener startButtonListener;
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.Register<Factory<Bullet>>(Lifetime.Singleton).Build();
-            builder.Register<Factory<Enemy>>(Lifetime.Singleton).Build();
-            builder.Register<Pool<Enemy>>(Lifetime.Singleton).Build();
-            builder.Register<Pool<Bullet>>(Lifetime.Singleton).Build();
+            builder.RegisterComponentInHierarchy<GameManager>();
+            
+            builder.Register<CharacterDeathController>(Lifetime.Singleton);
+            builder.Register<GameCooldownLauncher>(Lifetime.Singleton).WithParameter(startTimerSettings)
+                .AsImplementedInterfaces().AsSelf();
+
+            RegisterInput(builder);
+            RegisterEnvironment(builder);
+            RegisterBulletElements(builder);
+            RegisterCharacterComponents(builder);
+            RegisterEnemyElements(builder);
+
+            RegisterUI(builder);
+            
+            builder.Register<GameContext>(Lifetime.Singleton);
+        }
+
+        private void RegisterUI(IContainerBuilder builder)
+        {
+            builder.RegisterInstance(pauseResumeButtonStateController).AsImplementedInterfaces();
+            builder.RegisterInstance(pauseResumeButtonListener).AsImplementedInterfaces();
+            builder.RegisterInstance(startButtonListener).AsImplementedInterfaces();
+        }
+
+        private void RegisterInput(IContainerBuilder builder)
+        {
+            builder.Register<KeyboardInput>(Lifetime.Singleton).AsImplementedInterfaces();
+        }
+
+        private void RegisterEnvironment(IContainerBuilder builder)
+        {
+            builder.RegisterComponentInHierarchy<LevelBounds>().AsSelf();
+            builder.RegisterInstance(backgroundMoveConfig);
+            builder.Register<LevelBackground>(Lifetime.Singleton).WithParameter(backgroundTransform)
+                .AsImplementedInterfaces();
+        }
+
+        private void RegisterEnemyElements(IContainerBuilder builder)
+        {
+            /*builder.Register(_ =>
+            {
+                using (EnqueueParent(this))
+                {
+                    return Instantiate(enemyPrefab);
+                }
+            }, Lifetime.Transient);*/
+            builder.RegisterInstance(enemyPrefab);
+            builder.Register<Factory<Enemy>>(Lifetime.Singleton).WithParameter(this);
+            builder.Register<Pool<Enemy>>(Lifetime.Singleton).WithParameter(enemyPoolContainer);
+            builder.RegisterInstance(new EnemyPositions(enemySpawnPositions, enemyFirePositions));
+            builder.Register<EnemyCountdownSpawner>(Lifetime.Singleton).WithParameter(enemySpawnDelay)
+                .AsImplementedInterfaces().AsSelf();
+            builder.Register<EnemyManager>(Lifetime.Singleton).WithParameter(enemyMaxCount).AsImplementedInterfaces().AsSelf();
+            builder.Register<EnemySpawner>(Lifetime.Singleton).WithParameter(worldTransform).WithParameter(enemyMaxCount);
+        }
+
+        private void RegisterBulletElements(IContainerBuilder builder)
+        {
+            builder.RegisterInstance(bulletPrefab);
+            //builder.RegisterComponentInNewPrefab(bulletPrefab, Lifetime.Transient);
+            builder.Register<Factory<Bullet>>(Lifetime.Singleton);
+            builder.Register<Pool<Bullet>>(Lifetime.Singleton).WithParameter(bulletPoolContainer);
+            builder.Register<BulletSpawnManager>(Lifetime.Singleton)
+                .AsImplementedInterfaces().AsSelf().WithParameter(worldTransform);
+            builder.Register<BulletLifecycleController>(Lifetime.Singleton)
+                .AsImplementedInterfaces().AsSelf();
+            builder.Register<BulletSystem>(Lifetime.Singleton);
+        }
+
+        private void RegisterCharacterComponents(IContainerBuilder builder)
+        {
+            builder.RegisterComponent(character).AsImplementedInterfaces().AsSelf();
+            builder.Register<CharacterDeathObserver>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
+            builder.Register<CharacterFireController>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
+            builder.Register<CharacterMoveController>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
+            builder.RegisterInstance(playerBulletConfig);
         }
     }
 }

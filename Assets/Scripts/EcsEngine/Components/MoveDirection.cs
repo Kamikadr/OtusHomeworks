345a0ce81a9c8128388badd2a9d03f1738d8d1f6
Kamikadr <60061947+Kamikadr@users.noreply.github.com>
2024-01-22 15:16:31 +0700

    add LeoEcs

---
+++ Assets/Scripts/EcsEngine/Components/MoveDirection.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Client.Components
+{
+    [Serializable]
+    public class MoveDirection
+    {
+        public Vector3 value;
+    }
+}
---
+++ Assets/Scripts/EcsEngine/Services/MovementSystem.cs
@@ -0,0 +1,13 @@
+using Leopotam.EcsLite;
+
+namespace Client.Services
+{
+    public class MovementSystem: IEcsRunSystem
+    {
+        //private 
+        public void Run(IEcsSystems systems)
+        {
+            
+        }
+    }
+}
